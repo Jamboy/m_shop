6de3880572867bcaddfd1d2de44cc2a54a4b3f90
Jamboy <jamboylive@gmail.com>
2021-06-29 13:36:24 +0800

    购物车

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,66 +1,56 @@
+/*
+ * @Description: 购物车
+ * @Author: Jamboy
+ * @Date: 2021-06-05 11:43:59
+ * @LastEditTime: 2021-06-29 10:48:50
+ */
 // pages/cart/cart.js
+import { Cart } from '../../models/cart'
+
 Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    cartItems: [],
+  },
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {},
 
-    },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+  onShow: function () {
+    const cart = new Cart()
+    const cartItems = cart.getALlCartItemFromLocal().items
+    if (cart.isEmpty()) {
+      this.empty()
+      return
     }
+
+    this.setData({
+      cartItems,
+    })
+    this.notEmpty()
+  },
+
+  empty() {
+    this.setData({
+      isEmpty: true,
+    })
+
+    wx.hideTabBarRedDot({
+      index: 2,
+    })
+  },
+
+  notEmpty() {
+    this.setData({
+      isEmpty: false,
+    })
+
+    wx.showTabBarRedDot({
+      index: 2,
+    })
+  },
 })
