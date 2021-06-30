c973b39f2661e6a5686ee88d086dab3ad0fd270b
Jamboy <jamboylive@gmail.com>
2021-06-30 09:02:25 +0800

    checkbox

--- components/checkBox/index.js
+++ components/checkBox/index.js
@@ -4,20 +4,30 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    checkBox: Boolean,
   },
 
   /**
    * 组件的初始数据
    */
-  data: {
-
-  },
+  data: {},
 
   /**
    * 组件的方法列表
    */
   methods: {
+    onChecked(e) {
+      let checked = this.properties.checked
 
-  }
+      this.setData({
+        checked: !this.properties.checked,
+      })
+
+      this.triggerEvent(
+        'check',
+        { checked: checked ? false : true },
+        { bubbles: true, complete: true }
+      )
+    },
+  },
 })
